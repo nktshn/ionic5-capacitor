dd29c79493d226ae124e9c46b62dab4b097ae3c9
Nikita Shishin <nktshn06@gmail.com>
2020-04-04 20:06:11 +0600

    dev: side menu refactoring

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { RoutingPaths } from 'src/app/routing-paths';
-import { SideMenuItem } from './menu.interface';
 import { Router } from '@angular/router';
+import { SideMenuService } from './side-menu.service';
 
 @Component({
   selector: 'app-side-menu',
@@ -10,27 +9,24 @@ import { Router } from '@angular/router';
 })
 export class SideMenuComponent implements OnInit {
 
-  menuItems: SideMenuItem[] = [
-    {
-      title: 'Profile',
-      ionIconName: 'person-circle-outline',
-      link: `${RoutingPaths.profile}`
-    },
-    {
-      title: 'Game store',
-      ionIconName: 'game-controller-outline',
-      link: `${RoutingPaths.main}`
-    },
-  ];
+  readonly menuId = 'main';
+
+  get menuItems() {
+    return this.sideMenuService.menuItems;
+  }
 
   constructor(
     private router: Router,
+    private sideMenuService: SideMenuService,
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+  }
 
   navigateTo(link: string): void {
     this.router.navigateByUrl(link);
+    this.sideMenuService.close(this.menuId);
   }
 
 }
---
+++ src/app/components/side-menu/side-menu.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { SideMenuItem } from './side-menu-item.interface';
+import { RoutingPaths } from 'src/app/routing-paths';
+import { MenuController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SideMenuService {
+  
+  readonly menuItems: SideMenuItem[] = [
+    {
+      title: 'Profile',
+      ionIconName: 'person-circle-outline',
+      link: `${RoutingPaths.profile}`
+    },
+    {
+      title: 'Game store',
+      ionIconName: 'game-controller-outline',
+      link: `${RoutingPaths.main}`
+    },
+  ];
+
+  constructor(
+    private menuCtrl: MenuController,
+  ) { }
+
+  close(menuId: string): Promise<boolean> {
+    return this.menuCtrl.close(menuId);
+  }
+}
